1af7d329c1acd730effa2fcb017c1037514ddf74
Hany Elmogey <h.elmogey@gmail.com>
2019-10-09 12:08:12 +0200

    Add files via upload

--- model.py
+++ model.py
@@ -12,7 +12,7 @@ def hidden_init(layer):
 class Actor(nn.Module):
     """Actor (Policy) Model."""
 
-    def __init__(self, state_size, action_size, seed, fc1_units=512,fc2_units=256):
+    def __init__(self, state_size, action_size, seed, fc1_units=512,fc2_units=512):
         """Initialize parameters and build model.
         Params
         ======
@@ -72,8 +72,8 @@ class Critic(nn.Module):
 
     def forward(self, state, action):
         """Build a critic (value) network that maps (state, action) pairs -> Q-values."""
-        xs = F.leaky_relu(self.bn(self.fcs1(state)))
+        xs = F.relu(self.bn(self.fcs1(state)))
         x = torch.cat((xs, action), dim=1)
-        x = F.leaky_relu(self.fc2(x))
-        x = F.leaky_relu(self.fc3(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
         return self.fc4(x)
